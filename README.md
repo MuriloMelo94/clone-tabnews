bfa224aaf5cadd8407b34a21a0e7826aae360be0
Murilo Melo <murilomelo94@outlook.com>
2023-08-17 19:25:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para aprender a implementar do início ao fim um projeto, partindo do zero.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>Mamãe crocheteira e Titias Crocheteiras, eu amo vocês. Se você me amam, deem uma risadinha! ❤️ </h1>
+function Home() {
+  return (
+    <h1>
+      Mamãe crocheteira e Titias Crocheteiras, eu amo vocês. Se você me amam,
+      deem uma risadinha! ❤️{" "}
+    </h1>
+  );
 }
 
 export default Home;
