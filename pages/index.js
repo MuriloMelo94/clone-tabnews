4d8e53a0f5e5e3edb3262cba54cd7a5fa3da13b0
Murilo Melo <murilomelo94@outlook.com>
2023-08-22 19:24:59 +0000

    altera `index.js` para `em construcao`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
 function Home() {
   return (
-    <h1>
-      Mamãe crocheteira e Titias Crocheteiras, eu amo vocês. Se você me amam,
-      deem uma risadinha! ❤️{" "}
-    </h1>
+    (
+      <h1>
+        Site murilomelo.dev <s>"Em Construção"</s>.
+      </h1>
+    ),
+    (
+      <p>
+        <s>Propósito:</s> ser o pedacinho da internet sob minha
+        responsabilidade, capaz de entregar meu valor profissional, de maneira
+        perceptível, para empresas e recrutadores.
+      </p>
+    ),
+    (
+      <p>
+        <s>Público-alvo:</s>Recrutadores, gestores e demais responsáveis por
+        contratações nas empresas.
+      </p>
+    ),
+    (
+      <h2>
+        <s>O que precisa ser:</s>
+      </h2>
+    ),
+    (
+      <ul>
+        <li>
+          Feito? Um portfólio da minha trajetória, resultado dos cursos que fiz,
+          deixar online os softwares das seleções empresariais que forem sendo
+          feitas
+        </li>
+        <li>Protegido?</li>
+        <li>Repetido?</li>
+      </ul>
+    )
   );
 }
 
